c12cecb6428d56d755f2b91300d6a04e09a5a333
alangthomas <alangthomas4@gmail.com>
2021-10-11 16:50:40 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddBooksComponent } from './components/add-books/add-books.component';
+import { AddCategoryComponent } from './components/add-category/add-category.component';
+import { AddSubcategoryComponent } from './components/add-subcategory/add-subcategory.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'home', pathMatch:'full'},
+  {path:'add-books', component:AddBooksComponent},
+  {path:'home', component:HomeComponent},
+  {path:'add-category', component: AddCategoryComponent},
+  {path:'add-subcategory', component:AddSubcategoryComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule }  from '@angular/common/http';
+import { FormsModule } from '@angular/forms'; 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
+import { AddCategoryComponent } from './components/add-category/add-category.component';
+import { AddSubcategoryComponent } from './components/add-subcategory/add-subcategory.component';
+import { AddBooksComponent } from './components/add-books/add-books.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminDashboardComponent,
+    AddCategoryComponent,
+    AddSubcategoryComponent,
+    AddBooksComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    HttpClientModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
