c3da7b5b54ec5ecaa7f4a241d42dccd14fda011e
alangthomas <alangthomas4@gmail.com>
2021-10-22 11:07:23 +0530

    styled homepage and books page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule }  from '@angular/common/http';
 import { FormsModule } from '@angular/forms'; 
+import {IvyCarouselModule} from 'angular-responsive-carousel';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -73,6 +74,7 @@ import { ViewAllOrdersComponent } from './components/view-all-orders/view-all-or
     NgbModule,
     HttpClientModule,
     FormsModule,
+    IvyCarouselModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
